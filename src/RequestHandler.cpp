bebf68b9659e20614ef4f780f865f5f0201b83a9
Felix Winter <ixos01@gmail.com>
2010-10-31 22:28:07 +0100

    Implemented first draft of the RequestHandler class

---
+++ src/RequestHandler.cpp
@@ -0,0 +1,48 @@
+/***************************************************************************
+* This file is part of libmygpo-qt                                         *
+* Copyright (c) 2010 Stefan Derkits <stefan@derkits.at>                    *
+* Copyright (c) 2010 Christian Wagner <christian.wagner86@gmx.at>          *
+* Copyright (c) 2010 Felix Winter <ixos01@gmail.com>                       *
+*                                                                          *
+* This library is free software; you can redistribute it and/or            *
+* modify it under the terms of the GNU Lesser General Public               *
+* License as published by the Free Software Foundation; either             *
+* version 2.1 of the License, or (at your option) any later version.       *
+*                                                                          *
+* This library is distributed in the hope that it will be useful,          *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU        *
+* Lesser General Public License for more details.                          *
+*                                                                          *
+* You should have received a copy of the GNU Lesser General Public         *
+* License along with this library; if not, write to the Free Software      *
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 *
+* USA                                                                      *
+***************************************************************************/
+
+#include "RequestHandler.h"
+
+RequestHandler RequestHandler::_instance;
+
+RequestHandler& RequestHandler::instance()
+{
+    return _instance;
+}
+
+QByteArray RequestHandler::getRequest(const QUrl& url)
+{
+	QNetworkRequest request(url);
+	QNetworkReply *reply = manager.get(request);
+	QEventLoop loop;
+	QObject::connect(reply, SIGNAL(readyRead()), &loop, SLOT(quit()));
+
+	// Execute the event loop here, now we will wait here until readyRead() signal is emitted
+	// which in turn will trigger event loop quit.
+	loop.exec();
+
+	return reply->readAll();
+}
+
+
+
+
