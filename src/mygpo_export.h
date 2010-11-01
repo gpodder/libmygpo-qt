61d0f8d428679730aeadcbaebede49e1846ed45c
Stefan Derkits <stefan@derkits.at>
2010-11-01 17:20:11 +0100

    added export header for visibillity Macro

---
+++ src/mygpo_export.h
@@ -0,0 +1,34 @@
+/***************************************************************************
+* This file is part of libmygpo-qt                                         *
+* Copyright (c) 2010 Stefan Derkits <stefan@derkits.at>                    *
+* Copyright (c) 2010 Christian Wagner <christian.wagner86@gmx.at>          *
+* Copyright (c) 2010 Felix Winter <ixos01@gmail.com>                       *
+*                                                                          *
+* This library is free software; you can redistribute it and/or            *
+* modify it under the terms of the GNU Lesser General Public               *
+* License as published by the Free Software Foundation; either             *
+* version 2.1 of the License, or (at your option) any later version.       *
+*                                                                          *
+* This library is distributed in the hope that it will be useful,          *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU        *
+* Lesser General Public License for more details.                          *
+*                                                                          *
+* You should have received a copy of the GNU Lesser General Public         *
+* License along with this library; if not, write to the Free Software      *
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 *
+* USA                                                                      *
+***************************************************************************/
+
+#ifndef MYGPO_EXPORT_H
+#define MYGPO_EXPORT_H
+
+#ifdef _WIN32
+  #define MYGO_EXPORT __declspec(dllexport)
+#elif __GNUC__ >= 4
+  #define MYGPO_EXPORT __attribute__ ((visibility("default")))
+#else
+  #define MYGPO_EXPORT
+#endif
+
+#endif // MYGPO_EXPORT_H
