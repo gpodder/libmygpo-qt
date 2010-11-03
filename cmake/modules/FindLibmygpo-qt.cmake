c82a2ed44325ef115f906ba757e658f3427b843f
Christian Wagner <christian.wagner86@gmx.at>
2010-11-04 00:24:08 +0100

    added cmake file to find libmygpo-qt, removed LibFindMacros.cmake

--- cmake/modules/FindLibmygpo-qt.cmake
+++ cmake/modules/FindLibmygpo-qt.cmake
@@ -1,11 +1,43 @@
-# -- Try to find libmygpo-qt --
-# Needs LibFindMacros.cmake in the same or in the CMake Modules Path
-# If successfull it will set the following Variables:
-# mygpo-qt_FOUND - libmygpo-qt is installed on the System
-# mygpo-qt_INCLUDE_DIRS - libmygpo-qt Include Directories
-# mygpo-qt_LIBRARIES - link these to use libmygpo-qt
 
-include(LibFindMacros)
+# - Find LibLastFM
+# Find the liblastfm includes and the liblastfm libraries
+# This module defines
+# LIBLASTFM_INCLUDE_DIR, root lastfm include dir
+# LIBLASTFM_LIBRARY, the path to liblastfm
+# LIBLASTFM_FOUND, whether liblastfm was found
 
-#Dependencies
 
+find_path(LIBMYGPO_INCLUDE_DIR NAMES mygpo.h
+   HINTS
+   ~/usr/include
+   /opt/local/include
+   /usr/include
+   /usr/local/include
+   /opt/kde4/include
+  ~/kde/include
+  PATH_SUFFIXES mygpo-qt
+)
+
+find_library( LIBMYGPO_LIBRARY NAMES mygpo-qt
+    PATHS
+    ~/usr/lib
+   /opt/local/lib
+   /usr/lib
+   /usr/lib64
+   /usr/local/lib
+   /opt/kde4/lib
+    ~/kde/lib
+)
+
+
+if(LIBMYGPO_INCLUDE_DIR AND LIBMYGPO_LIBRARY)
+   set(LIBMYGPO_FOUND TRUE)
+   message(STATUS "Found liblastfm: ${LIBMYGPO_INCLUDE_DIR}, ${LIBMYGPO_LIBRARY}")
+else(LIBMYGPO_INCLUDE_DIR AND LIBMYGPO_LIBRARY)
+   set(LIBMYGPO_FOUND FALSE)   
+   if (LIBMYGPO_FIND_REQUIRED)
+      message(FATAL_ERROR "Could NOT find required package LibLastFm")
+   endif(LIBMYGPO_FIND_REQUIRED)
+endif(LIBMYGPO_INCLUDE_DIR AND LIBMYGPO_LIBRARY)
+
+mark_as_advanced(LIBMYGPO_INCLUDE_DIR LIBMYGPO_LIBRARY)
