4ed87be74fb27d9c2f48c5957ffdd321273f58eb
Stefan Derkits <stefan@derkits.at>
2010-11-25 12:33:03 +0100

    RequestHandler Support for async calls

--- src/RequestHandler.cpp
+++ src/RequestHandler.cpp
@@ -27,34 +27,27 @@
 
 using namespace mygpo;
 
-RequestHandler::RequestHandler(const QString& username, const QString& password) : m_errorFlag(QNetworkReply::NoError), m_username(username), m_password(password), m_loginFailed(false)
+RequestHandler::RequestHandler(const QString& username, const QString& password) : m_username(username), m_password(password), m_loginFailed(false)
 {
     QObject::connect(&manager, SIGNAL(authenticationRequired(QNetworkReply*,QAuthenticator*)), this,
                      SLOT(authenticate( QNetworkReply*, QAuthenticator*)));
 }
 
-RequestHandler::RequestHandler() : m_errorFlag(QNetworkReply::NoError), m_username(), m_password(), m_loginFailed(false)
+RequestHandler::RequestHandler() : m_username(), m_password(), m_loginFailed(false)
 {
 }
 
-int RequestHandler::getRequest(QByteArray& response, const QUrl& url)
+QNetworkReply* RequestHandler::getRequest(QByteArray& response, const QUrl& url)
 {
     m_loginFailed = false;
-    m_errorFlag = QNetworkReply::NoError;
     QNetworkRequest request(url);
-    QNetworkReply *reply = manager.get(request);
-
-    waitForReply(*reply);
-
-    if (m_errorFlag == QNetworkReply::NoError) {
-        response = reply->readAll();
-    }
-    return m_errorFlag;
+    QNetworkReply* reply = manager.get(request);
+    return reply;
 }
 
 
 
-int RequestHandler::postRequest( QByteArray& response, const QByteArray& data, const QUrl& url )
+QNetworkReply* RequestHandler::postRequest(const QByteArray& data, const QUrl& url )
 {
     /*NOTE: using the same variable for error handling as getRequest.
      * (If we want to implement asynchronous calls for get/post-Request, use
@@ -62,34 +55,12 @@ int RequestHandler::postRequest( QByteArray& response, const QByteArray& data, c
      * Also use a copy of data for asynchronous calls!
      */
     m_loginFailed = false;
-    m_errorFlag = QNetworkReply::NoError;
     QNetworkRequest request( url );
-    QNetworkReply *reply = manager.post( request, data );
+    QNetworkReply* reply = manager.post( request, data );
 
-    waitForReply(*reply);
 
-    if ( m_errorFlag == QNetworkReply::NoError ) {
-        response = reply->readAll();
-    }
-    return m_errorFlag;
+    return reply;
 }
-
-void RequestHandler::waitForReply( const QNetworkReply& reply )
-{
-    QEventLoop loop;
-    QObject::connect( &reply, SIGNAL( finished() ), &loop, SLOT( quit() ) );
-    QObject::connect( &reply, SIGNAL( error( QNetworkReply::NetworkError ) ), this,
-                      SLOT( handleError( QNetworkReply::NetworkError ) ) );
-    QObject::connect( &reply, SIGNAL( error( QNetworkReply::NetworkError ) ), &loop,
-                      SLOT( quit() ) );
-    loop.exec();
-}
-
-void RequestHandler::handleError(QNetworkReply::NetworkError code)
-{
-    m_errorFlag = code;
-}
-
 
 void RequestHandler::authenticate( QNetworkReply* reply, QAuthenticator* authenticator )
 {
@@ -101,8 +72,3 @@ void RequestHandler::authenticate( QNetworkReply* reply, QAuthenticator* authent
         authenticator->setPassword(m_password);
     }
 }
-
-
-
-
-
