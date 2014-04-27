900f7a5714b823f3f79bef937589d33d78190cb6
Stefan Derkits <stefan@derkits.at>
2014-04-27 19:16:31 +0200

    check if JsonDocument is null in JsonWrapper

--- src/qjsonwrapper/Json.cpp
+++ src/qjsonwrapper/Json.cpp
@@ -111,7 +111,7 @@ toJson( const QVariant &variant, bool* ok )
     QJsonDocument doc = QJsonDocument::fromVariant( variant );
     if ( ok != NULL )
     {
-        *ok = true;
+        *ok = !doc.isNull();
     }
     return doc.toJson( QJsonDocument::Compact );
 #else
